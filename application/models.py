cc96a18c9d80af4ed48c39a7ebd31592b5c41c87
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-02-13 14:20:15 +0200

    Initial commit

--- application/models.py
+++ application/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Link(models.Model):
+    long_url = models.TextField()
+    short_url = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='links')
+
+    class Meta:
+        ordering = ('created_at', )
